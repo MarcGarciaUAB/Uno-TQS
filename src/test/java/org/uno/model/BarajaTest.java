0cb44f5b69bcb387a94a5d01592606e9375d424e
1673889 <1673889@uab.cat>
2025-11-20 11:57:14 +0100

    archivos nuevos + archivos test

---
+++ src/test/java/org/uno/model/BarajaTest.java
@@ -0,0 +1,9 @@
+package org.uno.model;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BarajaTest {
+  @Test
+  void testCrearBaraja(){
+  }
+}
---
+++ src/test/java/org/uno/model/PilaTest.java
@@ -0,0 +1,9 @@
+package org.uno.model;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PilaTest {
+  @Test
+  void testCrearPila(){
+  }
+}
