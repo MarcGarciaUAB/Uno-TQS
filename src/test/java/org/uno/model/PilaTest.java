0c719c00442d101d914f359ebb92eaf83909e158
1673889 <1673889@uab.cat>
2025-11-26 09:06:56 +0100

    primeros tests Pila

--- src/test/java/org/uno/model/PilaTest.java
+++ src/test/java/org/uno/model/PilaTest.java
@@ -1,9 +1,22 @@
 package org.uno.model;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PilaTest {
+  Pila pila;
+  @BeforeEach
+  void setUp() {
+    pila = new Pila();
+  }
   @Test
   void testCrearPila(){
+    assertNotEquals(null, pila);
   }
 }
+
+  void testJugarCarta(){
+    Carta cartaJugada = new Carta(5, "Rojo");
+    pila.jugarCarta(cartaJugada);
+    assertEquals(cartaJugada, pila.ultimaCarta());
+  }
