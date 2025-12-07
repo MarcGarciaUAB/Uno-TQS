f160677d079688688406cbe74871769fbdc48207
MarcGarciaUAB <marc.uab013@gmail.com>
2025-12-07 21:05:21 +0100

    funcion controller más sólida

--- src/main/java/org/uno/controlador/GameController.java
+++ src/main/java/org/uno/controlador/GameController.java
@@ -5,18 +5,22 @@ import org.uno.model.*;
 import java.util.List;
 
 public class GameController {
-  private Baraja baraja;
-  private Pila pila;
-  private List<Mano> jugadores;
+    private Baraja baraja;
+    private Pila pila;
+    private List<Mano> jugadores;
 
-  public GameController(Baraja baraja, Pila pila, List<Mano> jugadores) {
-    this.baraja = baraja;
-    this.pila = pila;
-    this.jugadores = jugadores;
-  }
+    public GameController(Baraja baraja, Pila pila, List<Mano> jugadores) {
+      this.baraja = baraja;
+      this.pila = pila;
+      this.jugadores = jugadores;
+    }
 
-  public boolean esCartaValida(Carta carta, Mano jugador) {
-    Carta cartaEnPila = pila.ultimaCarta();
-    return jugador.tieneCartaJugable(cartaEnPila);
+    public boolean esCartaValida(Carta carta, Mano jugador) {
+      Carta cartaEnPila = pila.ultimaCarta();
+      if (jugador.tieneCartaJugable(cartaEnPila)) {
+        return carta.mismoColor(cartaEnPila) || carta.mismoValor(cartaEnPila);
+      }
+      else
+        return false;
+    }
   }
-}
