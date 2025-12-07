ad220eeba1c621bb96fd5748f8c2bde9f156046e
MarcGarciaUAB <marc.uab013@gmail.com>
2025-12-07 20:43:23 +0100

    test barajar completo

--- src/test/java/org/uno/model/BarajaTest.java
+++ src/test/java/org/uno/model/BarajaTest.java
@@ -7,18 +7,19 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class BarajaTest {
-  private List<Carta> barajada;
+  private List<Carta> barajada = new ArrayList<>();
   private Krupier mockKrupier;
   Baraja baraja;
 
   @BeforeEach
   void setUp() {
     mockKrupier = mock(Krupier.class);
-    baraja = new Baraja();
+    baraja = new Baraja(mockKrupier);
   }
 
   @Test
@@ -26,13 +27,13 @@ public class BarajaTest {
     assertNotEquals(null, baraja);
   }
 
-
-  @Test
+//TODO: pre/post condiciones que hagan esto.
+  /*@Test
   void testBarajaInicialTiene108Cartas() {
     //cuatro cambia color, cuatro +4, cuatro 0, ocho 1-9 y ocho block, reverse y +2
     assertEquals(108, baraja.getNumeroCartas());
   }
-
+*/
   @Test
   void testBarajaTieneColoresCorrectos() {
     List<Carta> cartas = baraja.getBaraja();
@@ -49,14 +50,34 @@ public class BarajaTest {
   @Test
   void testBarajar() {
     //declaramos la baraja con las cartas específicas que queremos
-    barajada.add(new Carta(0, "Rojo"));
-    List<Carta> antes = List.copyOf(baraja.getBaraja());
+    barajada = baraja.getBaraja();
+    List<Carta> antes = baraja.getBaraja();
+    //esto busca la posición de la carta que queremos, en este caso un +4
+    Carta cartaEspecial = null;
+    for (Carta c : barajada) {
+      if ("Negro".equals(c.getColor()) && "+4".equals(c.getEfecto())) {
+        cartaEspecial = c;
+        break;
+      }
+    }
+//la ponemos al principio
+    if (cartaEspecial != null) {
+      barajada.remove(cartaEspecial);
+      barajada.add(0, cartaEspecial);
+    }
+
     when(mockKrupier.barajar(antes)).thenReturn(barajada);
     baraja.barajar();
 
     List<Carta> despues = baraja.getBaraja();
-
+//comprobamos que no es lo mismo
     assertNotEquals(antes, despues);
+    Carta primera = baraja.robar();
+    //nos aseguramos de que el mock ha barajado bien
+    assertEquals("+4", primera.getEfecto());
+    assertEquals("Negro", primera.getColor());
+    //y que se ha creado correctamente con el tamaño adecuado
+    assertEquals(107, baraja.tamaño());
   }
 
   @Test
