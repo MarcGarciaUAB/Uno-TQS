18b2cb43d2dd6903d7061acde5a097af7226b5ab
MarcGarciaUAB <marc.uab013@gmail.com>
2025-12-08 10:56:31 +0100

    método en public y no private (whoops)

--- src/main/java/org/uno/controlador/GameController.java
+++ src/main/java/org/uno/controlador/GameController.java
@@ -64,7 +64,7 @@ public class GameController {
       jugador.añadirCarta(robada);
     return false;
   }
-  private void aplicarEfecto(Carta carta, Mano jugador) {
+  public void aplicarEfecto(Carta carta, Mano jugador) {
     if (carta.getEfecto() == null) return;
 
     switch (carta.getEfecto()) {
